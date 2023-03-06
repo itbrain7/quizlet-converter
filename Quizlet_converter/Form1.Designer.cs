a92ed928c8e49805c1fe50c30c7e9f5823bd7acd
JISIK-BRAIN1 <brain@jisikcorp.com>
2023-03-06 17:16:08 +0900

    Add project files.

---
+++ Quizlet_converter/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace Quizlet_converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_openfile = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button_convert = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button_opendirectory = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // button_openfile
+            // 
+            this.button_openfile.Location = new System.Drawing.Point(40, 24);
+            this.button_openfile.Name = "button_openfile";
+            this.button_openfile.Size = new System.Drawing.Size(222, 41);
+            this.button_openfile.TabIndex = 0;
+            this.button_openfile.Text = "파일 선택";
+            this.button_openfile.UseVisualStyleBackColor = true;
+            this.button_openfile.Click += new System.EventHandler(this.button_openfile_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(40, 86);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(723, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button_convert
+            // 
+            this.button_convert.Location = new System.Drawing.Point(40, 250);
+            this.button_convert.Name = "button_convert";
+            this.button_convert.Size = new System.Drawing.Size(222, 39);
+            this.button_convert.TabIndex = 2;
+            this.button_convert.Text = "컨버팅하기";
+            this.button_convert.UseVisualStyleBackColor = true;
+            this.button_convert.Click += new System.EventHandler(this.button_convert_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(40, 295);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(723, 143);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button_opendirectory
+            // 
+            this.button_opendirectory.Location = new System.Drawing.Point(282, 24);
+            this.button_opendirectory.Name = "button_opendirectory";
+            this.button_opendirectory.Size = new System.Drawing.Size(222, 41);
+            this.button_opendirectory.TabIndex = 4;
+            this.button_opendirectory.Text = "디렉토리 선택";
+            this.button_opendirectory.UseVisualStyleBackColor = true;
+            this.button_opendirectory.Click += new System.EventHandler(this.button_opendirectory_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_opendirectory);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button_convert);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button_openfile);
+            this.Name = "Form1";
+            this.Text = "Quizlet Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_openfile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button_convert;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button_opendirectory;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ Quizlet_converter/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quizlet_converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_openfile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            //openFileDialog.DefaultExt = "html";
+            //openFileDialog.Filter = "Html Files(*.html)|Text Files(*.txt;*.ini)";
+            openFileDialog.ShowDialog();
+            if (openFileDialog.FileName.Length > 0)
+            {
+                foreach (string filename in openFileDialog.FileNames)
+                {
+					log("Selected file : " + filename);
+					this.textBox1.Text = filename;
+                }
+            }       
+        }
+
+		private void button_opendirectory_Click(object sender, EventArgs e)
+		{
+
+			using (var fbd = new FolderBrowserDialog())
+			{
+				DialogResult result = fbd.ShowDialog();
+
+				if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+				{
+					this.textBox1.Text = fbd.SelectedPath;
+
+					string[] files = Directory.GetFiles(fbd.SelectedPath);
+
+					log("Selected directory : " + fbd.SelectedPath);
+					log("Files found: " + files.Length.ToString());
+				}
+			}
+		}
+
+		private void button_convert_Click(object sender, EventArgs e)
+        {
+			string input_file = textBox1.Text;
+
+			if (input_file.Length==0)
+            {
+				log("파일이나 디렉토리를 먼저 선택해주세요.", true);
+				return;
+            }
+
+			if (Directory.Exists(input_file))
+            {
+				button_convert.Enabled = false;
+				int cnt=start_directory(input_file);
+				button_convert.Enabled = true;
+				log("디렉토리안의 파일 : " + cnt + "개가 컨버팅 완료되었습니다.", true);
+			}
+			else if (File.Exists(input_file))
+            {
+				button_convert.Enabled = false;
+				string output_file=start_file(input_file);
+				button_convert.Enabled = true;
+				log("컨버팅 완료되었습니다 : " + output_file, true);
+			}
+			else
+            {
+				log("파일 경로가 유효하지 않습니다.", true);
+				return;
+            }
+        }
+
+        private void log(String s, bool showMessage)
+        {
+            textBox2.AppendText(s);
+            textBox2.AppendText("\r\n");
+			if (showMessage)
+            {
+				MessageBox.Show(s);
+            }
+        }
+
+		private void log(String s)
+        {
+			log(s, false);
+        }
+
+		private void log(int i)
+        {
+			log(i.ToString(), false);
+        }
+
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private string replace_first(string src, string old_value, string new_value)
+        {
+			int found = src.IndexOf(old_value);
+			if (found >= 0)
+			{
+				string temp = src.Substring(0, src.IndexOf(old_value));
+				string temp2 = src.Substring(src.IndexOf(old_value) + old_value.Length);
+				return temp + new_value + temp2;
+			}
+			return src;
+		}
+
+		/// <summary>
+		/// 문자열을 시작문자열과 끝문자열을 기준으로 잘라서, 크기가 3인 문자열배열을 리턴한다.
+		/// (시작문자열 못찾으면 크기가 1인 문자열배열, 시작문자열은 찾고, 끝문자열을 못찾으면 크기가 2인 문자열)
+		/// </summary>
+		/// <param name="src">원문자열</param>
+		/// <param name="string_start">찾는시작 문자열</param>
+		/// <param name="string_end">찾기끝 문자열 </param>
+		/// <returns></returns>
+		private string[] splits(string src, string string_start, string string_end)
+        {
+
+			if (src == null) return null;
+
+			int found_start, found_end, cut_end;
+			
+			found_start = src.IndexOf(string_start);
+			if (found_start < 0) return new string[] { src };
+
+			string str_first = src.Substring(0, found_start);
+			found_end = src.IndexOf(string_end, found_start + string_start.Length);
+
+			if (found_end < 0) return new string[] { str_first, src.Substring(found_start + string_start.Length) };
+			cut_end = found_end + string_end.Length;
+
+			return new string[] { str_first, src.Substring(found_start + string_start.Length, found_end-found_start- (string_start.Length) ), src.Substring(cut_end) };		
+		}
+
+		private int start_directory(string input_dir)
+        {
+			if (!Directory.Exists(input_dir))
+            {
+				log("Directory is not existed (" + input_dir + ")", true);
+				return 0;
+			}
+
+			int cnt = 0;
+
+			foreach (string input_file in Directory.GetFiles(input_dir)) 
+            {
+				start_file(input_file);
+				cnt++;
+            }
+
+			return cnt;
+        }
+
+		private string start_file(string input_file) 
+		{		
+		
+			if (Directory.Exists(input_file)) { 
+				log("input_dir is not a file(" + input_file + ")");
+				return null;
+			}
+
+			string str = System.IO.File.ReadAllText(input_file);
+
+			string output_dir = System.IO.Directory.GetParent(input_file) + @"\Quizlet_converted";
+			if (!Directory.Exists(output_dir))
+            {
+				Directory.CreateDirectory(output_dir);
+            }
+			
+			string output_file = output_dir + @"\" + Path.GetFileName(input_file);
+			
+			string new_str = str;
+			string LINE = "<!------------------------------------>";
+		
+			while (true) {
+
+				//log("new_str.Length=" + new_str.Length);
+				string[] ss = splits(new_str, "<div style=\"display:none\">", "</div>");
+				if (ss.Length>=3) {				
+					String new_s1 = ss[1];
+				
+					if (new_s1.IndexOf("</span><span>")>=0) {				
+					
+						while (true) {
+							String new_s1_changed = replace_first(new_s1, "</span><span>", "\t");
+							new_s1_changed=replace_first(new_s1_changed, "</span><span>", "\r\n");
+							if (new_s1==new_s1_changed) {
+								break;
+							}
+							new_s1 = new_s1_changed;
+						}
+					
+						new_s1 = new_s1.Replace("<span>", "\r\n");
+						new_s1 = new_s1.Replace("</span>", "\r\n");
+						new_str = ss[0] + "\r\n" + LINE + "\r\n" + "<div style=\"display:none\" leedo>" + new_s1 + "</div>\r\n" + LINE + "\r\n" + ss[2];
+					}
+					else
+					{
+						new_str = ss[0] + new_s1 + ss[2];
+					}
+				}
+				else
+				{
+					break;
+				}
+			}
+
+			File.WriteAllText(output_file, new_str);
+			return output_file;
+		}
+
+    }
+}
