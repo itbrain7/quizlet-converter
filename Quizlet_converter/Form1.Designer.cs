391544c78b7866bfca1f4d8d82ffc15d50f1bdc0
JISIK-BRAIN1 <brain@jisikcorp.com>
2023-03-07 14:18:59 +0900

    1st

--- Quizlet_converter/Form1.Designer.cs
+++ Quizlet_converter/Form1.Designer.cs
@@ -36,11 +36,13 @@ namespace Quizlet_converter
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button_opendirectory = new System.Windows.Forms.Button();
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.SuspendLayout();
             // 
             // button_openfile
             // 
-            this.button_openfile.Location = new System.Drawing.Point(40, 24);
+            this.button_openfile.Location = new System.Drawing.Point(12, 9);
             this.button_openfile.Name = "button_openfile";
             this.button_openfile.Size = new System.Drawing.Size(222, 41);
             this.button_openfile.TabIndex = 0;
@@ -54,14 +56,14 @@ namespace Quizlet_converter
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(40, 86);
+            this.textBox1.Location = new System.Drawing.Point(12, 71);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(723, 23);
+            this.textBox1.Size = new System.Drawing.Size(760, 23);
             this.textBox1.TabIndex = 1;
             // 
             // button_convert
             // 
-            this.button_convert.Location = new System.Drawing.Point(40, 250);
+            this.button_convert.Location = new System.Drawing.Point(14, 250);
             this.button_convert.Name = "button_convert";
             this.button_convert.Size = new System.Drawing.Size(222, 39);
             this.button_convert.TabIndex = 2;
@@ -71,16 +73,16 @@ namespace Quizlet_converter
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(40, 295);
+            this.textBox2.Location = new System.Drawing.Point(12, 295);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBox2.Size = new System.Drawing.Size(723, 143);
+            this.textBox2.Size = new System.Drawing.Size(760, 143);
             this.textBox2.TabIndex = 3;
             // 
             // button_opendirectory
             // 
-            this.button_opendirectory.Location = new System.Drawing.Point(282, 24);
+            this.button_opendirectory.Location = new System.Drawing.Point(264, 9);
             this.button_opendirectory.Name = "button_opendirectory";
             this.button_opendirectory.Size = new System.Drawing.Size(222, 41);
             this.button_opendirectory.TabIndex = 4;
@@ -88,11 +90,19 @@ namespace Quizlet_converter
             this.button_opendirectory.UseVisualStyleBackColor = true;
             this.button_opendirectory.Click += new System.EventHandler(this.button_opendirectory_Click);
             // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(242, 250);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(530, 39);
+            this.progressBar1.TabIndex = 5;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.button_opendirectory);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button_convert);
@@ -115,6 +125,8 @@ namespace Quizlet_converter
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button_opendirectory;
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
 
--- Quizlet_converter/Form1.cs
+++ Quizlet_converter/Form1.cs
@@ -13,6 +13,9 @@ namespace Quizlet_converter
 {
     public partial class Form1 : Form
     {
+
+		bool USE_PROGRESSBAR = false;
+
         public Form1()
         {
             InitializeComponent();
@@ -21,9 +24,14 @@ namespace Quizlet_converter
         private void button_openfile_Click(object sender, EventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
-            //openFileDialog.DefaultExt = "html";
-            //openFileDialog.Filter = "Html Files(*.html)|Text Files(*.txt;*.ini)";
-            openFileDialog.ShowDialog();
+			//openFileDialog.DefaultExt = "html";
+
+
+			//openFileDialog.Filter="Excel files (*.xls,*xlsx)|*.xls;*xlsx|All files (*.*)|*.*";
+
+			openFileDialog.Filter = "Html Files(*.html;*.htm)|*.html;*.htm|Text Files(*.txt;*.ini)|*.txt;*.text;*.ini|All files|*.*";
+
+			openFileDialog.ShowDialog();
             if (openFileDialog.FileName.Length > 0)
             {
                 foreach (string filename in openFileDialog.FileNames)
@@ -55,6 +63,12 @@ namespace Quizlet_converter
 
 		private void button_convert_Click(object sender, EventArgs e)
         {
+
+			if (USE_PROGRESSBAR)
+			{
+				progressBar1.Value = 0;
+			}
+
 			string input_file = textBox1.Text;
 
 			if (input_file.Length==0)
@@ -63,30 +77,68 @@ namespace Quizlet_converter
 				return;
             }
 
-			if (Directory.Exists(input_file))
-            {
-				button_convert.Enabled = false;
-				int cnt=start_directory(input_file);
-				button_convert.Enabled = true;
-				log("디렉토리안의 파일 : " + cnt + "개가 컨버팅 완료되었습니다.", true);
-			}
-			else if (File.Exists(input_file))
-            {
-				button_convert.Enabled = false;
-				string output_file=start_file(input_file);
-				button_convert.Enabled = true;
-				log("컨버팅 완료되었습니다 : " + output_file, true);
-			}
-			else
+			if (!Directory.Exists(input_file) && !File.Exists(input_file))
             {
 				log("파일 경로가 유효하지 않습니다.", true);
 				return;
-            }
-        }
+			}
 
-        private void log(String s, bool showMessage)
+			button_convert.Enabled = false;
+			backgroundWorker1.RunWorkerAsync();
+		}
+
+		void worker_DoWork(object sender, DoWorkEventArgs e)
         {
-            textBox2.AppendText(s);
+
+			string input_file = textBox1.Text;
+
+			if (Directory.Exists(input_file)) {
+				int cnt = start_directory(input_file);				
+			}
+			else
+			{
+				string output_file = start_file(input_file, true);
+			}			
+		}
+
+		// Progress 리포트 - UI Thread
+		void worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+		{
+			if (e.ProgressPercentage < 100) { 
+				if (USE_PROGRESSBAR) this.progressBar1.Value = e.ProgressPercentage;
+			}
+		}
+
+		// 작업 완료 - UI Thread
+		void worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+		{
+
+			// 에러가 있는지 체크
+			if (e.Error != null)
+			{
+				log(e.Error.Message);
+			}
+			else
+            {
+				string input_file = textBox1.Text;
+
+				if (USE_PROGRESSBAR) progressBar1.Value = 100;
+
+				if (Directory.Exists(input_file))
+				{
+					log("입력한 디렉토리안의 파일 컨버팅이 완료되었습니다.", true);
+				}
+				else
+				{
+					log("입력한 파일 컨버팅 완료되었습니다 : ", true);
+				}
+			}
+			button_convert.Enabled = true;
+		}
+
+		private void log(String s, bool showMessage)
+        {
+			textBox2.AppendText(DateTime.Now.ToString("HH:mm:ss ") + s);
             textBox2.AppendText("\r\n");
 			if (showMessage)
             {
@@ -107,26 +159,39 @@ namespace Quizlet_converter
 
 		private void Form1_Load(object sender, EventArgs e)
 		{
+			
+			backgroundWorker1.WorkerReportsProgress = true;
+			backgroundWorker1.WorkerSupportsCancellation = true;
+			backgroundWorker1.DoWork += new DoWorkEventHandler(worker_DoWork);
+			backgroundWorker1.ProgressChanged += new ProgressChangedEventHandler(worker_ProgressChanged);
+			backgroundWorker1.RunWorkerCompleted += new RunWorkerCompletedEventHandler(worker_RunWorkerCompleted);
 
 		}
 
+		/// <summary>
+		/// 문자열에서 "찾을
+		/// </summary>
+		/// <param name="src">원본 문자열</param>
+		/// <param name="old_value">찾을 문자열</param>
+		/// <param name="new_value">변경할 문자열</param>
+		/// <returns></returns>
 		private string replace_first(string src, string old_value, string new_value)
         {
 			int found = src.IndexOf(old_value);
 			if (found >= 0)
 			{
-				string temp = src.Substring(0, src.IndexOf(old_value));
-				string temp2 = src.Substring(src.IndexOf(old_value) + old_value.Length);
-				return temp + new_value + temp2;
+				string temp_first = src.Substring(0, src.IndexOf(old_value));
+				string temp_last = src.Substring(src.IndexOf(old_value) + old_value.Length);
+				return temp_first + new_value + temp_last;
 			}
 			return src;
 		}
 
 		/// <summary>
-		/// 문자열을 시작문자열과 끝문자열을 기준으로 잘라서, 크기가 3인 문자열배열을 리턴한다.
+		/// 원본문자열을 시작문자열과 끝문자열을 기준으로 잘라서, 크기가 3인 문자열배열을 리턴한다.
 		/// (시작문자열 못찾으면 크기가 1인 문자열배열, 시작문자열은 찾고, 끝문자열을 못찾으면 크기가 2인 문자열)
 		/// </summary>
-		/// <param name="src">원문자열</param>
+		/// <param name="src">원본 문자열</param>
 		/// <param name="string_start">찾는시작 문자열</param>
 		/// <param name="string_end">찾기끝 문자열 </param>
 		/// <returns></returns>
@@ -153,41 +218,46 @@ namespace Quizlet_converter
         {
 			if (!Directory.Exists(input_dir))
             {
-				log("Directory is not existed (" + input_dir + ")", true);
-				return 0;
+				return -1;
 			}
 
-			int cnt = 0;
+			string[] files = Directory.GetFiles(input_dir);
+			int seq = 0;
 
-			foreach (string input_file in Directory.GetFiles(input_dir)) 
+			foreach (string input_file in files) 
             {
-				start_file(input_file);
-				cnt++;
+				seq++;
+				start_file(input_file, false);
+				backgroundWorker1.ReportProgress(seq * 100 / files.Length);
             }
 
-			return cnt;
+			return files.Length;
         }
 
-		private string start_file(string input_file) 
+		private string start_file(string input_file, bool only_one) 
 		{		
 		
-			if (Directory.Exists(input_file)) { 
-				log("input_dir is not a file(" + input_file + ")");
+			if (!File.Exists(input_file)) { 
 				return null;
 			}
 
+			if (only_one) backgroundWorker1.ReportProgress(10);
+
 			string str = System.IO.File.ReadAllText(input_file);
 
-			string output_dir = System.IO.Directory.GetParent(input_file) + @"\Quizlet_converted";
+			string output_dir = Path.Combine(System.IO.Directory.GetParent(input_file).FullName, @"Quizlet_converted_" + DateTime.Now.ToString("yyyyMMdd_HHmm"));
+
 			if (!Directory.Exists(output_dir))
             {
 				Directory.CreateDirectory(output_dir);
             }
 			
-			string output_file = output_dir + @"\" + Path.GetFileName(input_file);
+			string output_file = Path.Combine(output_dir, Path.GetFileName(input_file));
 			
 			string new_str = str;
 			string LINE = "<!------------------------------------>";
+
+			int step=0;
 		
 			while (true) {
 
@@ -195,10 +265,18 @@ namespace Quizlet_converter
 				string[] ss = splits(new_str, "<div style=\"display:none\">", "</div>");
 				if (ss.Length>=3) {				
 					String new_s1 = ss[1];
-				
-					if (new_s1.IndexOf("</span><span>")>=0) {				
+
 					
+					if (new_s1.IndexOf("</span><span>")>=0) {
+
+						if (only_one)
+						{
+							++step;
+							if (step<=8) backgroundWorker1.ReportProgress(step * 10);
+						}
+
 						while (true) {
+							
 							String new_s1_changed = replace_first(new_s1, "</span><span>", "\t");
 							new_s1_changed=replace_first(new_s1_changed, "</span><span>", "\r\n");
 							if (new_s1==new_s1_changed) {
@@ -222,7 +300,12 @@ namespace Quizlet_converter
 				}
 			}
 
+			if (only_one) backgroundWorker1.ReportProgress(90);
+
 			File.WriteAllText(output_file, new_str);
+
+			if (only_one) backgroundWorker1.ReportProgress(100);
+
 			return output_file;
 		}
 
